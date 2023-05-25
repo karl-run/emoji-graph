3ca5aeebe35c7332d7e32700115fb8f2d9f9e6ad
Karl J. Overå <k@rl.run>
2023-05-25 09:23:02 +0200

    add some quick stats

--- src/components/AccessibleTextSummary.tsx
+++ src/components/AccessibleTextSummary.tsx
@@ -27,20 +27,42 @@ function AccessibleTextSummary({
   );
 
   return (
-    <details>
-      <summary>Accessible text for screenshots</summary>
-      <h4>Title:</h4>
-      <pre className="border p-1">
-        Top {size} emoji uploaders between {start.toLocaleDateString()} and{' '}
-        {end.toLocaleDateString()}
-      </pre>
-      <h4>Description:</h4>
-      <pre className="border p-1">
-        {R.reverse(top)
-          .map(([name, count]) => `${name}: ${count}`)
-          .join('\n')}
-      </pre>
-    </details>
+    <>
+      <details>
+        <summary>Accessible text for screenshots</summary>
+        <h4>Title:</h4>
+        <pre className="border p-1">
+          Top {size} emoji uploaders between {start.toLocaleDateString()} and{' '}
+          {end.toLocaleDateString()}
+        </pre>
+        <h4>Description:</h4>
+        <pre className="border p-1">
+          {R.reverse(top)
+            .map(([name, count]) => `${name}: ${count}`)
+            .join('\n')}
+        </pre>
+      </details>
+      <details>
+        <summary>Fun stats</summary>
+        <dl>
+          <dt>Total emojis</dt>
+          <dd>{emoji.length}</dd>
+          <dt>Unqiue uploaders</dt>
+          <dd>{R.uniqBy(emoji, (it) => it.user_id).length}</dd>
+          <dt>Uploaders with more than 10</dt>
+          <dd>
+            {
+              R.pipe(
+                emoji,
+                R.groupBy(R.prop('user_id')),
+                R.toPairs,
+                R.filter(([_, it]) => it.length > 10),
+              ).length
+            }
+          </dd>
+        </dl>
+      </details>
+    </>
   );
 }
 
